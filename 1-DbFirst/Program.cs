67f2352b41903f28304ffaab969573ac86759429
yunusky <yunusky@outlook.com>
2024-09-19 11:39:39 +0300

    Add project files.

---
+++ 1-DbFirst/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _1_DbFirst
+{
+	internal class Program
+	{
+		#region Gerekli ortemin hazirlanmasi 
+		/*
+		 * 
+		 * 1 - EntityFramework tool 'unu kurmak
+		 * bunu komut satiri uzerinden yapacağiz
+		 * 
+		 * dotnet tool install --global dotnet-ef
+		 * 
+		 * Ayrica projenize EntityFrameworkCore paketini ve hangi database ile calisacak isek 
+		 * ilgili mesela EntityFrameworkCore.SqlServer
+		 * paketini ve EntityFrameworkCore.Design paketini kurmak gereklidir.
+		 * 
+		 * 
+		 * 
+		 */
+		#endregion
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+}
